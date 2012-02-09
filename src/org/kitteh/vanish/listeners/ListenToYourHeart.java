e8909610ab88700c359fc1183c8fe913fdb1c6b1
Matt Baxter <matt@phozop.net>
2012-02-08 22:51:44 -0500

    Paintings fix

---
+++ src/org/kitteh/vanish/listeners/ListenToYourHeart.java
@@ -0,0 +1,32 @@
+package org.kitteh.vanish.listeners;
+
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.painting.PaintingBreakByEntityEvent;
+import org.bukkit.event.painting.PaintingBreakEvent;
+import org.kitteh.vanish.VanishPerms;
+import org.kitteh.vanish.VanishPlugin;
+
+public class ListenToYourHeart implements Listener {
+
+    private final VanishPlugin plugin;
+
+    public ListenToYourHeart(VanishPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void badReview(PaintingBreakEvent criticism) {
+        if (criticism instanceof PaintingBreakByEntityEvent) {
+            final Entity critic = ((PaintingBreakByEntityEvent) criticism).getRemover();
+            if (critic instanceof Player) {
+                final Player arrogantFool = (Player) critic;
+                if (this.plugin.getManager().isVanished(arrogantFool) && VanishPerms.canNotInteract(arrogantFool)) {
+                    criticism.setCancelled(true);
+                }
+            }
+        }
+    }
+}
