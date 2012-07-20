1cea603545e9470b79311ed8b38c5c785f885da1
Matt Baxter <matt@phozop.net>
2012-07-19 22:46:14 -0400

    Event priority

--- src/org/kitteh/vanish/listeners/TagAPIListener.java
+++ src/org/kitteh/vanish/listeners/TagAPIListener.java
@@ -2,6 +2,7 @@ package org.kitteh.vanish.listeners;
 
 import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.kitteh.tag.PlayerReceiveNameTagEvent;
 import org.kitteh.vanish.VanishPlugin;
@@ -13,7 +14,7 @@ public class TagAPIListener implements Listener {
         this.plugin = instance;
     }
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGHEST)
     public void onNameTag(PlayerReceiveNameTagEvent event) {
         if (this.plugin.getManager().isVanished(event.getNamedPlayer())) {
             event.setTag(ChatColor.AQUA + event.getNamedPlayer().getName());
