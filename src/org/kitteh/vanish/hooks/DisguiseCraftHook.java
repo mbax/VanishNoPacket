1ec4c788dea738adcddff5dc3261d4ddd2730f3c
Matt Baxter <matt@phozop.net>
2012-08-05 23:21:39 -0400

    Clean up disguisecraft hook

--- src/org/kitteh/vanish/hooks/DisguiseCraftHook.java
+++ src/org/kitteh/vanish/hooks/DisguiseCraftHook.java
@@ -6,57 +6,50 @@ import org.bukkit.event.*;
 import org.bukkit.plugin.Plugin;
 import org.kitteh.vanish.VanishPlugin;
 import org.kitteh.vanish.event.VanishStatusChangeEvent;
-import org.kitteh.vanish.staticaccess.VanishNoPacket;
-import org.kitteh.vanish.staticaccess.VanishNotLoadedException;
 
 import pgDev.bukkit.DisguiseCraft.DisguiseCraft;
 import pgDev.bukkit.DisguiseCraft.api.DisguiseCraftAPI;
 import pgDev.bukkit.DisguiseCraft.api.PlayerDisguiseEvent;
 
 public class DisguiseCraftHook extends Hook implements Listener {
-    
+
     private DisguiseCraftAPI dcAPI = null;
 
     public DisguiseCraftHook(VanishPlugin plugin) {
         super(plugin);
     }
-    
+
     @Override
     public void onEnable() {
-        Plugin disguiseCraft = plugin.getServer().getPluginManager().getPlugin("DisguiseCraft");
-        if (disguiseCraft == null) {
-            plugin.log("You wanted DisguiseCraft support. I could not find DisguiseCraft.");
-        } else {
-            plugin.log("Now hooking into DisguiseCraft");
-            dcAPI = DisguiseCraft.getAPI();
-            plugin.getServer().getPluginManager().registerEvents(this, plugin);
+        final Plugin disguiseCraft = this.plugin.getServer().getPluginManager().getPlugin("DisguiseCraft");
+        if (disguiseCraft != null) {
+            this.plugin.log("Now hooking into DisguiseCraft");
+            this.dcAPI = DisguiseCraft.getAPI();
+            this.plugin.getServer().getPluginManager().registerEvents(this, this.plugin);
         }
     }
-    
+
     @EventHandler
     public void beforeVanishChange(VanishStatusChangeEvent event) {
-        if(dcAPI == null){
+        if (this.dcAPI == null) {
             return;
         }
-        Player player = event.getPlayer();
-        if (dcAPI.isDisguised(player)) {
-            dcAPI.undisguisePlayer(player);
-            player.sendMessage(ChatColor.AQUA + "You have been undisguised for vanishing.");
+        final Player player = event.getPlayer();
+        if (this.dcAPI.isDisguised(player)) {
+            this.dcAPI.undisguisePlayer(player);
+            player.sendMessage(ChatColor.AQUA + "You have been undisguised for toggling vanishing.");
         }
     }
-    
+
     @EventHandler
     public void onDisguise(PlayerDisguiseEvent event) {
-        if(dcAPI == null){
+        if (this.dcAPI == null) {
             return;
         }
-        Player player = event.getPlayer();
-        try {
-            if (VanishNoPacket.isVanished(player.getName())) {
-                VanishNoPacket.toggleVanishSilent(player);
-                player.sendMessage(ChatColor.AQUA + "You have been unvanished for disguising.");
-            }
-        } catch (VanishNotLoadedException e) {
+        final Player player = event.getPlayer();
+        if (this.plugin.getManager().isVanished(player.getName())) {
+            this.plugin.getManager().toggleVanishQuiet(player, false);
+            player.sendMessage(ChatColor.AQUA + "You have been unvanished for toggling disguising.");
         }
     }
 
