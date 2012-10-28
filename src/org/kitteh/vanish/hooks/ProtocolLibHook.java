85bd7fc6aedecbbea8f264ba620829ebb5b9bde1
mbax <matt@phozop.net>
2012-10-28 18:08:30 -0400

    Add ProtocolLib support for 1.4 motd.
    Meant to do this before first 1.4 release. Oops.

--- src/org/kitteh/vanish/hooks/ProtocolLibHook.java
+++ src/org/kitteh/vanish/hooks/ProtocolLibHook.java
@@ -29,16 +29,34 @@ public class ProtocolLibHook extends Hook {
                 public void onPacketSending(PacketEvent event) {
                     try {
                         final StructureModifier<String> stringModifier = event.getPacket().getSpecificModifier(String.class);
-                        final String[] replyString = stringModifier.read(0).split(ChatColor.COLOR_CHAR + "");
-                        if (replyString.length == 3) {
+                        final String replyString = stringModifier.read(0);
+                        int offset = 0;
+                        String splitter = String.valueOf(ChatColor.COLOR_CHAR); // 1.3 and earlier
+                        if (replyString.startsWith(splitter)) { // 1.4 and onward
+                            splitter = "\u0000";
+                            offset = 3;
+                        }
+                        final String[] split = replyString.split(splitter);
+                        if (split.length == (3 + offset)) {
                             int online;
                             try {
-                                online = Integer.parseInt(replyString[1]);
+                                online = Integer.parseInt(split[1 + offset]);
                             } catch (final NumberFormatException e) {
                                 return;
                             }
                             online -= ProtocolLibHook.this.plugin.getManager().numVanished();
-                            stringModifier.write(0, replyString[0] + ChatColor.COLOR_CHAR + online + ChatColor.COLOR_CHAR + replyString[2]);
+                            final StringBuilder builder = new StringBuilder();
+                            for (int x = 0; x < split.length; x++) {
+                                if (builder.length() > 0) {
+                                    builder.append(splitter);
+                                }
+                                if (x == (1 + offset)) {
+                                    builder.append(online);
+                                    continue;
+                                }
+                                builder.append(split[x]);
+                            }
+                            stringModifier.write(0, builder.toString());
                         }
                     } catch (final FieldAccessException e) {
                     }
